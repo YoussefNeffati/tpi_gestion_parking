f0a80850d7c63f4ba79968dd5293beb14dc802ce
Prince Divin MACKPAYEN <prince.mackpayen@abeeway.com>
2022-12-07 15:41:06 +0100

    Creation  of the application navbar and the different pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,23 @@
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Report from './pages/Report';
+import Notifications from './pages/Notifications';
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/report' component={Report} />
+          <Route path='/notifications' component={Notifications} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
